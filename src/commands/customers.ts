5413c5671b3988806d5eb959b8a9667b9c3960c8
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2022-09-12 13:27:48 +0200

    repzo

--- src/commands/customers.ts
+++ src/commands/customers.ts
@@ -1,9 +1,21 @@
 import { CommandEvent } from "../types";
+import Repzo from "repzo";
 
 // @ts-ignore
 import QuickBooks from "node-quickbooks";
 
 export const customers = async (commandEvent: CommandEvent) => {
+  const repzo = new Repzo(commandEvent.app.formData?.repzoApiKey, {
+    env: commandEvent.env,
+  });
+
+  const commandLog = new Repzo.CommandLog(
+    repzo,
+    commandEvent.app,
+    commandEvent.command
+  );
+
+  console.log(repzo);
   var qbo = new QuickBooks(
     null,
     null,
@@ -11,25 +23,24 @@ export const customers = async (commandEvent: CommandEvent) => {
     true, // no token secret for oAuth 2.0
     commandEvent.oAuth2.realmId,
     true, // use the sandbox?
-    true, // enable debugging?
+    false, // enable debugging?
     null, // set minorversion, or null for the latest version
     "2.0", //oAuth version
     ""
   );
 
-  // qbo.findCustomers(
-  //   {
-  //     fetchAll: true,
-  //   },
-  //   function (e: any, customers: any) {
-  //     if (e) console.log(e);
-  //     console.log(customers);
-  //   }
-  // );
-
-  let ress = await qbo.findCustomers({
-    fetchAll: true,
-  });
-
-  console.log(ress);
+  qbo.findCustomers(
+    {
+      fetchAll: true,
+    },
+    function (e: any, result: any) {
+      if (e) throw e;
+      const customers = JSON.stringify(result.QueryResponse, null, 4);
+      // console.log(customers);
+      commandLog
+        .setStatus("success")
+        .setBody("Complete test QuickBooks custommers Sync")
+        .commit();
+    }
+  );
 };
