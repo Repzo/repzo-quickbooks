6511b845929edf46d519a3305d38359f34b000cb
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2022-10-11 13:54:22 +0200

    manage async calls

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # repzo-quickbooks
 
 repzo-quickbooks integration
+
+lets you connect QuickBooks Online with repzo apps, so you can automate your work and have more time for what matters most—no code required.
+
+### Quickbooks Modules
+
+- Customers
+- Items
+- Taxs
+- Invoices
--- src/commands/taxs.ts
+++ src/commands/taxs.ts
@@ -40,45 +40,77 @@ export const taxs = async (commandEvent: CommandEvent): Promise<Result> => {
     failed: 0,
   };
 
-  try {
-    await commandLog.load(command_sync_id);
-    await commandLog
-      .addDetail("Repzo QuickBooks: Started Syncing Taxs")
-      .commit();
+  return new Promise<Result>(async (resolve, reject) => {
+    try {
+      await commandLog.load(command_sync_id);
+      await commandLog
+        .addDetail("Repzo QuickBooks: Started Syncing Taxs")
+        .commit();
 
-    if (!app.options_formData[bench_time_key]) {
-      await commandLog.addDetail("bench_time_taxs undefined").commit();
+      if (!app.options_formData[bench_time_key]) {
+        await commandLog.addDetail("bench_time_taxs undefined").commit();
+      }
+
+      // return all repzo taxs
+      let qb_taxs = await get_all_QuickBooks_taxs(qbo);
+      // return all quickbooks taxs
+      let repzo_taxs = await get_all_repzo_taxs(repzo);
+
+      Promise.all(get_promisify_jobs(qb_taxs, repzo_taxs, repzo))
+        .then((values) => {
+          result.sync = values.length;
+          commandLog
+            .addDetail(
+              `Complete : Sync ${values.length} TaxRate with Quickbooks`
+            )
+            .setBody(result)
+            .commit();
+
+          resolve(result);
+        })
+        .catch((err) => {
+          console.error(`failed to complete sync due to an exception : ${err}`);
+          commandLog
+            .setStatus("fail", "failed to complete sync due to an exception")
+            .setBody(result)
+            .commit();
+          reject(result);
+        });
+    } catch (err) {
+      await commandLog
+        .setStatus("fail", "failed to complete sync due to an exception")
+        .setBody(result)
+        .commit();
+      reject(result);
     }
+  });
+};
 
-    // return all repzo taxs
-    let qb_taxs = await get_all_QuickBooks_taxs(qbo);
-    // return all quickbooks taxs
-    let repzo_taxs = await get_all_repzo_taxs(repzo);
-
-    Promise.all(promisify(qb_taxs, repzo_taxs, repzo))
-      .then((values) => {
-        result.sync = values.length;
-        commandLog
-          .addDetail(
-            `Complete : Sync ${values.length} Item / Product with Quickbooks ,and bench_time  ${commandEvent.app.options_formData[bench_time_key]}`
-          )
-          .setBody(result)
-          .commit();
-      })
-      .catch((err) => {
-        console.error(`failed to complete sync due to an exception : ${err}`);
-        commandLog
-          .setStatus("fail", "failed to complete sync due to an exception")
-          .setBody(result)
-          .commit();
-      });
-  } catch (err) {
-    await commandLog
-      .setStatus("fail", "failed to complete sync due to an exception")
-      .setBody(result)
-      .commit();
-  }
-  return result;
+const get_promisify_jobs = (
+  qb_taxs: TaxRate.Find.Result,
+  repzo_taxs: Service.Tax.Get.Result[],
+  repzo: Repzo
+): any[] => {
+  let jobs: any[] = []; // save all jobs here
+  qb_taxs.QueryResponse.TaxRate.forEach((tax, index, array) => {
+    let existTax = repzo_taxs.filter(
+      (i) => i.integration_meta?.QuickBooks_id === tax.Id
+    );
+    if (existTax[0]) {
+      if (
+        new Date(existTax[0]?.integration_meta?.QuickBooks_last_sync) <
+        new Date(tax.MetaData?.LastUpdatedTime)
+      ) {
+        let repzo_tax = map_taxs(tax);
+        jobs.push(repzo.tax.update(existTax[0]._id, repzo_tax));
+      }
+    } else {
+      //create a new  repzo client
+      let repzo_tax = map_taxs(tax);
+      jobs.push(repzo.tax.create(repzo_tax));
+    }
+  });
+  return jobs;
 };
 
 const get_all_repzo_taxs = async (
@@ -109,33 +141,6 @@ const get_all_QuickBooks_taxs = async (
   } catch (err) {
     throw err;
   }
-};
-
-const promisify = (
-  qb_taxs: TaxRate.Find.Result,
-  repzo_taxs: Service.Tax.Get.Result[],
-  repzo: Repzo
-): any[] => {
-  let jobs: any[] = []; // save all jobs here
-  qb_taxs.QueryResponse.TaxRate.forEach((tax, index, array) => {
-    let existTax = repzo_taxs.filter(
-      (i) => i.integration_meta?.QuickBooks_id === tax.Id
-    );
-    if (existTax[0]) {
-      if (
-        new Date(existTax[0]?.integration_meta?.QuickBooks_last_sync) <
-        new Date(tax.MetaData?.LastUpdatedTime)
-      ) {
-        let repzo_tax = map_taxs(tax);
-        jobs.push(repzo.tax.update(existTax[0]._id, repzo_tax));
-      }
-    } else {
-      //create a new  repzo client
-      let repzo_tax = map_taxs(tax);
-      jobs.push(repzo.tax.create(repzo_tax));
-    }
-  });
-  return jobs;
 };
 
 const map_taxs = (tax: TaxRate.TaxRateObject): Service.Tax.Create.Body => {
